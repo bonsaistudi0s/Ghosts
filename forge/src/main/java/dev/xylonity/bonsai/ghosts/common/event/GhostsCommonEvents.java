6baa62199df3626663728ac8f5909ff0771fe151
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-12-18 01:55:31 +0100

    🍦 Now ghosts respawn their owner even on expected unloaded chunks

--- forge/src/main/java/dev/xylonity/bonsai/ghosts/common/event/GhostsCommonEvents.java
+++ forge/src/main/java/dev/xylonity/bonsai/ghosts/common/event/GhostsCommonEvents.java
@@ -1,66 +1,44 @@
 package dev.xylonity.bonsai.ghosts.common.event;
 
 import dev.xylonity.bonsai.ghosts.Ghosts;
-import dev.xylonity.bonsai.ghosts.GhostsForge;
-import dev.xylonity.bonsai.ghosts.common.entity.ghost.GhostEntity;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
+import dev.xylonity.bonsai.ghosts.common.entity.AbstractGhostEntity;
+import dev.xylonity.bonsai.ghosts.util.GhostOwnerTracker;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Ghosts.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class GhostsCommonEvents {
 
     @SubscribeEvent
-    public void onDeath(LivingDeathEvent event) {
-        LivingEntity living = event.getEntity();
-        Level world = living.level();
-
-        if (living instanceof Player player) {
-            List<GhostEntity> ghosts = world.getEntitiesOfClass(GhostEntity.class, new AABB(living.blockPosition().offset(-10, -10, -10), living.blockPosition().offset(10, 10, 10)), EntitySelector.LIVING_ENTITY_STILL_ALIVE);
-
-            for (GhostEntity ghost : ghosts) {
-                if (ghost.isOwnedBy(living)) {
-                    if (ghost.getHoldItem().getItem() == Items.TOTEM_OF_UNDYING) {
-                        event.setCanceled(true);
-                        defaultTotemBehavior(player);
-
-                        ghost.setHoldItem(ItemStack.EMPTY);
-
-                        break;
-                    }
+    public static void onDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (player.level() instanceof ServerLevel serverLevel) {
+                AbstractGhostEntity ghost = GhostOwnerTracker.getInstance().findGhostWithTotem(serverLevel, player.getUUID());
+                if (ghost != null) {
+                    defaultTotemBehavior(player);
+                    ghost.getMainHandItem().shrink(1);
+                    event.setCanceled(true);
                 }
+
             }
 
         }
 
     }
 
-    private void defaultTotemBehavior(LivingEntity entity) {
-        entity.setHealth(1.0F);
+    private static void defaultTotemBehavior(LivingEntity entity) {
+        entity.setHealth(1F);
         entity.removeAllEffects();
         entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 900, 1));
         entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 100, 1));
-
-        GhostsForge.PROXY.createTrackingEmitter(entity, ParticleTypes.TOTEM_OF_UNDYING, 30);
-
-        entity.level().playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.TOTEM_USE, SoundSource.PLAYERS, 1.0F, 1.0F);
-        entity.playSound(SoundEvents.TOTEM_USE, 1.0F, 1.0F);
-
-        GhostsForge.PROXY.displayItemActivation(new ItemStack(Items.TOTEM_OF_UNDYING));
+        entity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 800, 0));
+        entity.level().broadcastEntityEvent(entity, (byte) 35);
     }
 
 }
