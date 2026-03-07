0bd368502f44b17f80872e882520b650c9316676
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-03-07 20:38:49 +0100

    ✋ Updated some config values

--- common/src/main/java/dev/xylonity/bonsai/ghosts/config/GhostsConfig.java
+++ common/src/main/java/dev/xylonity/bonsai/ghosts/config/GhostsConfig.java
@@ -9,17 +9,17 @@ public final class GhostsConfig {
     @ConfigEntry(
             comment = "Ghost spawnrate: [weight, minAmount, maxAmount, biomes and tags... (as many as you want)]"
     )
-    public static String GHOST_SPAWN = "14, 1, 2, #minecraft:is_forest";
+    public static String GHOST_SPAWN = "8, 1, 2, #minecraft:is_forest";
 
     @ConfigEntry(
             comment = "Ghost spawnrate: [weight, minAmount, maxAmount, biomes and tags... (as many as you want)]"
     )
-    public static String SMALL_GHOST_SPAWN = "14, 1, 3, minecraft:flower_forest, #minecraft:is_forest, #forge:is_swamp";
+    public static String SMALL_GHOST_SPAWN = "8, 1, 2, minecraft:flower_forest, #minecraft:is_forest, #forge:is_swamp";
 
     @ConfigEntry(
             comment = "Kodama spawnrate: [weight, minAmount, maxAmount, biomes and tags... (as many as you want)]"
     )
-    public static String KODAMA_SPAWN = "40, 1, 4, minecraft:birch_forest, minecraft:forest, minecraft:old_growth_spruce_taiga, minecraft:dark_forest";
+    public static String KODAMA_SPAWN = "20, 1, 2, minecraft:birch_forest, minecraft:forest, minecraft:old_growth_spruce_taiga, minecraft:dark_forest";
 
     @ConfigEntry(
             comment = "The maximum distance (in blocks) at which ghosts will teleport to their owner.",
